180ad917739cb2479b60b1fc8317c232ba6a1044
Сабака Сутулая <40202917+kapertdog@users.noreply.github.com>
2023-02-23 20:12:50 +0500

    Readme

---
+++ readme.md
@@ -0,0 +1,19 @@
+<center>
+
+<!--
+![shrimp.svg](icon%2Fshrimp.svg)
+-->
+
+# Shrimp 🦐
+#### Wanna some invisible roles? 🌌
+
+</center>
+
+## About
+> _soon™_
+
+## Invite link
+> not available yet
+
+## Thanks
+> _soon™_
---
+++ shrimp/tokenEncrypt.py
@@ -0,0 +1,37 @@
+import getpass
+import cryptocode
+
+
+def get(pin: str = ""):
+    try:
+        with open("token", "r+", encoding="UTF-8") as file:
+            _token = file.read()
+    except Exception as a:
+        print(a)
+        return create()[0]
+    if not pin:
+        pin = getpass.getpass("Enter pincode\n> ")
+    while not cryptocode.decrypt(_token, pin):
+        pin = getpass.getpass("Wrong pincode! Try again\n> ")
+    return cryptocode.decrypt(_token, pin)
+
+
+def create(token: str = ""):
+    print("Enter bot token to continue, or press CTRL+C to exit")
+    while not len(token) == 72:
+        print("It's must be 72 characters")
+        token = getpass.getpass("> ").lstrip().rstrip()
+    print("Ok! Now create a PIN-CODE to protect your token.\n"
+          "You need to enter it each time you start this bot")
+    pin = ""
+    while not len(pin) >= 4:
+        print("It's must be more than 4 characters")
+        pin = getpass.getpass("> ")
+    with open("token", "w+", encoding="UTF-8") as file:
+        file.write(cryptocode.encrypt(token, pin))
+    return token, pin
+
+
+if __name__ == "__main__":
+    print(get())
+    ...
